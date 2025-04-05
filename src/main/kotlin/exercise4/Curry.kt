a5bc99cf586e5d85cda7a57e1e2c79b689532289
Hyunsik Kang <hyunsik.kang@snowcorp.com>
2025-04-05 16:01:09 +0900

    fix: formatting

--- src/main/kotlin/exercise4/Curry.kt
+++ src/main/kotlin/exercise4/Curry.kt
@@ -3,9 +3,8 @@ package exercise4
 fun sum(num1: Int, num2: Int) = num1 + num2
 fun strConcat(s1: String, s2: String) = "$s1 $s2"
 
-fun <A,B,C> ((A,B) -> C).curry(): (A) -> (B) -> (C) = { a ->
+fun <A, B, C> ((A, B) -> C).curry(): (A) -> (B) -> (C) = { a ->
     { b -> this(a, b) }
 }
 
-infix fun <A,B> ((A) -> B).`+++`(a: A): B = this(a)
-
+infix fun <A, B> ((A) -> B).`+++`(a: A): B = this(a)
