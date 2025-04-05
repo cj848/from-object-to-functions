f6a1c03f1e0bd38d961f6cefb62958bf4448ddf3
Hyunsik Kang <hyunsik.kang@snowcorp.com>
2025-04-05 17:42:36 +0900

    feat: monoid 구현

--- src/main/kotlin/exercise5/Monoid.kt
+++ src/main/kotlin/exercise5/Monoid.kt
@@ -1,5 +1,5 @@
 package exercise5
 
-class Monoid<T : Any>(val initial: T, val operation: (T, T) -> T) {
+class Monoid<T : Any>(private val initial: T, private val operation: (T, T) -> T) {
     fun Collection<T>.fold(): T = this.fold(initial, operation)
 }
